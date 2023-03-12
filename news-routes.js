8012c922118704aec5e24a6dedbe3ed9cc8df89a
ageorgief <andreyg673@gmail.com>
2023-03-12 19:24:23 +0200

    Add post route to news-routes.js

--- news-routes.js
+++ news-routes.js
@@ -11,6 +11,16 @@ router.get("/news/:id", async (context) => {
     context.body = news[id];
 });
 
+//Create Route
+router.post("/news", async (context) => {
+    const body = context.request.body;
+
+    newsObj = new News(body.id, body.date, body.title, body.description, body.text);
+    news.push(newsObj);
+
+    context.body = newsObj;
+})
+
 //Read Route 
 router.get("/news", async (context) => {
     context.body = news;
