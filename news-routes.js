67b00105002ed0d65f9f434bcb2ddede3edcf4f7
ageorgief <andreyg673@gmail.com>
2023-03-12 18:36:38 +0200

    Add get route for news

--- news-routes.js
+++ news-routes.js
@@ -1,7 +1,19 @@
 const Router  = require("koa-router");
 const bodyParser = require('koa-bodyparser');
+const { News, news } = require("./News");
 const router = new Router();
 
 router.use(bodyParser());
+
+//Read Route for id
+router.get("/news/:id", async (context) => {
+    const id = context.params.id;
+    context.body = news[id];
+});
+
+//Read Route 
+router.get("/news", async (context) => {
+    context.body = news;
+});
 
 module.exports = router;
